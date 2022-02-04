e7352dea3e74ff0dd24a1daee5dde3dbb5096a81
G <snsdkf6@gmail.com>
2022-02-04 17:53:35 +0900

    swing 레이아웃까지

---
+++ src/site/metacoding/ex13/FlowLayoutEx01.java
@@ -0,0 +1,33 @@
+package site.metacoding.ex13;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+// Crtl + F11 실행
+public class FlowLayoutEx01 extends MyFrame {
+
+	public FlowLayoutEx01() {
+		super(300,300); // 생략되어 있음.super는 항상 제일 위에
+		System.out.println("FlowLayoutEx01");
+		JPanel panel = (JPanel) getContentPane(); // JFrame의 패널 가져오기
+		panel.setLayout(new FlowLayout());
+
+		JButton btn1 = new JButton("버튼1");
+		JButton btn2 = new JButton("버튼2");
+		JButton btn3 = new JButton("버튼3");
+		JButton btn4 = new JButton("버튼4");
+		panel.add(btn1);
+		panel.add(btn2);
+		panel.add(btn3);
+		panel.add(btn4);
+		
+		setVisible(true); /// 그림그리기
+	}
+
+	public static void main(String[] args) {
+		FlowLayoutEx01 mf = new FlowLayoutEx01();
+	}
+
+}
---
+++ src/site/metacoding/ex13/constant/BorderConstant.java
@@ -0,0 +1,12 @@
+package site.metacoding.ex13.constant;
+
+public class BorderConstant {
+
+	public final static String NORTH = "North";
+	public final static String SOUTH = "South";
+	public final static String EAST = "East";
+	public final static String WEST = "West";
+	public final static String CENTER = "Center"; //final이 붙으면 대문자
+	
+
+}
